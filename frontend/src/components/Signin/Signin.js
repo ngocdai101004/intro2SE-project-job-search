c491b46b40f72606d284dfbc82f2ea12c093f243
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-11-06 19:19:54 +0700

    sign in ui

--- frontend/src/components/Signin/Signin.js
+++ frontend/src/components/Signin/Signin.js
@@ -1,11 +1,111 @@
+import "./Signin.scss";
 
+const Signin = () => {
+  return (
+    <div className="container d-flex vh-100">
+      <div className="row w-100">
+        <div className="signin-container m-auto col-5">
+          <div>
+            <div className="title col-12 mb-4">
+              <h3>Sign in</h3>
+            </div>
 
-const Signin = () => {  
-    return (
-        <div>
-            <h1>Sign in</h1>
+            <div className="m-3">
+              <div className="input-icons mb-3">
+                <i className="fa-solid fa-envelope"></i>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="email form-control"
+                />
+              </div>
+              <div className="input-icons">
+                <i className="fa-solid fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="password form-control"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-eye-off"
+                >
+                  <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
+                  <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+                  <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
+                  <path d="m2 2 20 20" />
+                </svg>
+              </div>
+              <span className="col-12 forgot-password">Forgot password?</span>
+              <button className="col-12 mb-3 mt-3 btn btn-primary signin-btn">
+                Sign in
+              </button>
+
+              {/* Draw line in span */}
+              <span className="col-12 mb-3 line">
+                <span>or</span>
+              </span>
+
+              <div className="other-signin-btn">
+                <button className="col-5 mx-2">
+                  <svg
+                    enable-background="new 0 0 48 48"
+                    height="24"
+                    viewBox="0 0 48 48"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m43.611 20.083h-1.611v-.083h-18v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657c-3.572-3.329-8.35-5.382-13.618-5.382-11.045 0-20 8.955-20 20s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
+                      fill="#ffc107"
+                    />
+                    <path
+                      d="m6.306 14.691 6.571 4.819c1.778-4.402 6.084-7.51 11.123-7.51 3.059 0 5.842 1.154 7.961 3.039l5.657-5.657c-3.572-3.329-8.35-5.382-13.618-5.382-7.682 0-14.344 4.337-17.694 10.691z"
+                      fill="#ff3d00"
+                    />
+                    <path
+                      d="m24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238c-2.008 1.521-4.504 2.43-7.219 2.43-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025c3.31 6.477 10.032 10.921 17.805 10.921z"
+                      fill="#4caf50"
+                    />
+                    <path
+                      d="m43.611 20.083h-1.611v-.083h-18v8h11.303c-.792 2.237-2.231 4.166-4.087 5.571.001-.001.002-.001.003-.002l6.19 5.238c-.438.398 6.591-4.807 6.591-14.807 0-1.341-.138-2.65-.389-3.917z"
+                      fill="#1976d2"
+                    />
+                  </svg>
+                </button>
+                <button className="col-5 mx-2">
+                  <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m23 12c0-6.07578-4.9242-11-11-11-6.07578 0-11 4.92422-11 11 0 5.4914 4.02187 10.0418 9.2812 10.8668v-7.6871h-2.79292v-3.1797h2.79292v-2.42344c0-2.75644 1.6415-4.27968 4.1551-4.27968 1.2032 0 2.4621.21484 2.4621.21484v2.70703h-1.3879c-1.3664 0-1.7917.84863-1.7917 1.71875v2.0625h3.0507l-.4877 3.1797h-2.563v7.6871c5.2593-.825 9.2812-5.3754 9.2812-10.8668z"
+                      fill="#1877f2"
+                    />
+                    <path
+                      d="m16.2818 15.1797.4877-3.1797h-3.0507v-2.0625c0-.87012.4253-1.71875 1.7917-1.71875h1.3879v-2.70703s-1.2589-.21484-2.4621-.21484c-2.5136 0-4.1551 1.52324-4.1551 4.27968v2.42344h-2.79292v3.1797h2.79292v7.6871c.5608.0881 1.1344.1332 1.7188.1332s1.158-.0451 1.7188-.1332v-7.6871z"
+                      fill="#fff"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Signin;
