50fbaae2ec1fbeb1aab11dc39ec46aaea486f6d1
fusodoya <fusodoya@gmail.com>
2024-12-19 21:02:53 +0700

    refactor navbar

--- frontend/src/components/MyHeader.tsx
+++ frontend/src/components/MyHeader.tsx
@@ -1,31 +1,64 @@
+import { Nav } from "react-bootstrap";
 
 export default function MyHeader() {
-    return (
-        < nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm" >
-            <div className="container">
-                <a className="navbar-brand" href="">Job search</a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav >
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+      <div
+        className="w-100 d-flex justify-content-between align-items-center px-3"
+        style={{ padding: "0 0" }}
+      >
+        <div className="d-flex align-items-center ms-3">
+          <a className="navbar-brand me-5" href="home">
+            <span>
+              <img src="\magnifier.png" alt="" style={{ height: 40 }} />
+              <img src="\Job Search.png" alt="" style={{ height: 30 }} />
+            </span>
+          </a>
+
+          <Nav variant="underline" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                Home
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                Company
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+
+        <div className="d-flex align-items-center">
+          <Nav variant="underline" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                Employers / Post Job
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+
+          <span className="text-white mx-3">|</span>
+
+          <Nav variant="underline" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                <i className="bi bi-chat-left text-white fs-5 me-2"></i>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                <i className="bi bi-bell text-white fs-5 me-2"></i>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1" href="/signin" className="text-white">
+                <i className="bi bi-person text-white fs-5 me-2"></i>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+      </div>
+    </nav>
+  );
 }
