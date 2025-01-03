985b4bf465c5544422e767b30fa98d54fab8a993
fusodoya <fusodoya@gmail.com>
2025-01-03 21:25:04 +0700

    config job UI

--- frontend/src/pages/Home/SearchBar.tsx
+++ frontend/src/pages/Home/SearchBar.tsx
@@ -1,41 +1,50 @@
 // SearchBar.tsx
-import React from 'react';
+import React from "react";
 
 interface SearchBarProps {
-  onSearch: (searchTerm: string) => void; 
+  onSearch: (searchTerm: string) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const handleSearch = () => {
-    const searchTerm = (document.getElementById('searchInput') as HTMLInputElement).value;
-    onSearch(searchTerm); 
+    const searchTerm = (
+      document.getElementById("searchInput") as HTMLInputElement
+    ).value;
+    onSearch(searchTerm);
   };
 
   return (
-    <div className="search-bar" style={{ display: 'flex', justifyContent: 'center', margin: '100px 100px 70px 100px' }}>
-      <input 
-        type="text" 
-        id="searchInput" 
-        placeholder="Search for jobs..." 
-        style={{ 
-          width: '500px', 
-          padding: '10px', 
-          borderRadius: '5px', 
-          border: '1px solid #ccc', 
-          boxShadow: '10px 2px 4px rgba(0, 0, 0, 0.1)' 
-        }} 
+    <div
+      className="search-bar"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "100px 100px 30px 100px",
+      }}
+    >
+      <input
+        type="text"
+        id="searchInput"
+        placeholder="Search for jobs..."
+        style={{
+          width: "500px",
+          padding: "10px",
+          borderRadius: "5px",
+          border: "1px solid #ccc",
+          boxShadow: "10px 2px 4px rgba(0, 0, 0, 0.1)",
+        }}
       />
-      <button 
+      <button
         onClick={handleSearch}
-        style={{ 
-          padding: '10px 20px', 
-          marginLeft: '10px', 
-          borderRadius: '5px', 
-          border: 'none', 
-          backgroundColor: '#007bff', 
-          color: '#fff', 
-          cursor: 'pointer', 
-          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' 
+        style={{
+          padding: "10px 20px",
+          marginLeft: "10px",
+          borderRadius: "5px",
+          border: "none",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          cursor: "pointer",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
         }}
       >
         Find jobs
