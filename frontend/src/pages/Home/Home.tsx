91d7a30b445d27b98f43f479c0056315b1696f7d
fusodoya <fusodoya@gmail.com>
2025-01-03 22:07:18 +0700

    add company creation

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import MyHeader from "../../components/MyHeader.tsx";
 import axiosInstance from "../../common/axiosInstance.tsx";
 // import {useNavigate} from "react-router-dom";
